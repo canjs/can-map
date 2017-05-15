8f001b612789694dced9e029d386293551cd709a
Bradley Momberger <brad@bitovi.com>
2017-05-15 16:58:44 -0400

    3.1.0-pre.3

---
+++ dist/amd/map-helpers.js
@@ -0,0 +1,106 @@
+/*can-map@3.1.0-pre.2#map-helpers*/
+define(function (require, exports, module) {
+    var isPlainObject = require('can-util/js/is-plain-object');
+    var isArray = require('can-util/js/is-array');
+    var isPromise = require('can-util/js/is-promise');
+    var CID = require('can-cid');
+    var types = require('can-types');
+    var assign = require('can-util/js/assign');
+    var madeMap = null;
+    var teardownMap = function () {
+        for (var cid in madeMap) {
+            if (madeMap[cid].added) {
+                delete madeMap[cid].obj._cid;
+            }
+        }
+        madeMap = null;
+    };
+    var mapHelpers = {
+        attrParts: function (attr, keepKey) {
+            if (keepKey) {
+                return [attr];
+            }
+            return typeof attr === 'object' ? attr : ('' + attr).split('.');
+        },
+        canMakeObserve: function (obj) {
+            return obj && !isPromise(obj) && (isArray(obj) || isPlainObject(obj));
+        },
+        serialize: function () {
+            var serializeMap = null;
+            return function (map, how, where) {
+                var cid = CID(map), firstSerialize = false;
+                if (!serializeMap) {
+                    firstSerialize = true;
+                    serializeMap = {
+                        attr: {},
+                        serialize: {}
+                    };
+                }
+                serializeMap[how][cid] = where;
+                map.each(function (val, name) {
+                    var result, isObservable = types.isMapLike(val), serialized = isObservable && serializeMap[how][CID(val)];
+                    if (serialized) {
+                        result = serialized;
+                    } else {
+                        if (map['___' + how]) {
+                            result = map['___' + how](name, val);
+                        } else {
+                            result = mapHelpers.getValue(map, name, val, how);
+                        }
+                    }
+                    if (result !== undefined) {
+                        where[name] = result;
+                    }
+                });
+                if (firstSerialize) {
+                    serializeMap = null;
+                }
+                return where;
+            };
+        }(),
+        getValue: function (map, name, val, how) {
+            if (types.isMapLike(val)) {
+                return val[how]();
+            } else {
+                return val;
+            }
+        },
+        define: null,
+        addComputedAttr: function (map, attrName, compute) {
+            map._computedAttrs[attrName] = {
+                compute: compute,
+                count: 0,
+                handler: function (ev, newVal, oldVal) {
+                    map._triggerChange(attrName, 'set', newVal, oldVal, ev.batchNum);
+                }
+            };
+        },
+        addToMap: function addToMap(obj, instance) {
+            var teardown;
+            if (!madeMap) {
+                teardown = teardownMap;
+                madeMap = {};
+            }
+            var hasCid = obj._cid;
+            var cid = CID(obj);
+            if (!madeMap[cid]) {
+                madeMap[cid] = {
+                    obj: obj,
+                    instance: instance,
+                    added: !hasCid
+                };
+            }
+            return teardown;
+        },
+        getMapFromObject: function (obj) {
+            return madeMap && madeMap[obj._cid] && madeMap[obj._cid].instance;
+        },
+        twoLevelDeepExtend: function (destination, source) {
+            for (var prop in source) {
+                destination[prop] = destination[prop] || {};
+                assign(destination[prop], source[prop]);
+            }
+        }
+    };
+    module.exports = exports = mapHelpers;
+});
